7527a0314cd5633eacb3b812d1d9e9b390261689
Lingesh1776 <lingeshwaransivanenthira@gmail.com>
2025-10-30 22:06:51 +0530

    practicing

---
+++ app2.js
@@ -0,0 +1,9 @@
+// we create nested elements by passing in the third argument of createElement
+// we give array of children as well
+const parent = React.createElement("div",{},
+    React.createElement("div",{},
+        React.createElement("h1",{},"kick start react")));
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
