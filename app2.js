1bdd75154b3c5e66556d3c5285d0338355a7b985
Lingesh1776 <lingeshwaransivanenthira@gmail.com>
2025-11-14 09:43:40 +0530

    reorder

--- app2.js
+++ app2.js
@@ -1,5 +1,8 @@
 // we create nested elements by passing in the third argument of createElement
 // we give array of children as well
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{},
     React.createElement("div",{},
         React.createElement("h1",{},"kick start react")));
